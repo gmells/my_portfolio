55350e0e602687e349ec68e1fc1971672e08a101
gmells <victormells30@gmail.com>
2024-10-18 22:55:44 +0100

    Add new div with multiple stayling

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -82,6 +82,13 @@ export const BentoGridItem = ({
             <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
           </BackgroundGradientAnimation>
         )}
+
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-1o"
+          )}
+        ></div>
       </div>
 
       <div className="group-hover/bento:translate-x-2 transition duration-200">
