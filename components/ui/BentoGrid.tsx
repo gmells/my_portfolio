aa60973ec42db4a0639aba9cd9af67761dd94092
gmells <victormells30@gmail.com>
2024-10-23 17:37:54 +0100

    create new div element for 6th id

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -125,6 +125,12 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}></div>
+            </div>
+          )}
         </div>
       </div>
     </div>
