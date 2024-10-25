2da335021cc29d32deee9ac5b3ba3b598b8f17fd
gmells <victormells30@gmail.com>
2024-10-25 18:54:23 +0100

    Create RecentProjects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import RecentProjects from "@/components/ui/RecentProjects";
 
 import { FaHome } from "react-icons/fa";
 
@@ -16,6 +17,7 @@ export default function Home() {
         />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
