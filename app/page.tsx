4c90a2a0497e4fd0fedcf81cdbea27b855085f62
gmells <victormells30@gmail.com>
2024-10-29 18:49:12 +0000

    add ckients page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Clients from "@/components/Clients";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -17,6 +18,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
