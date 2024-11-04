0e734e4cb5e7373c6eb1da70c2f32ef7cfc2719a
gmells <victormells30@gmail.com>
2024-11-04 02:57:57 +0000

    edit page file styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -12,7 +14,7 @@ export default function Home() {
   return (
     <main
       className="relative bg-black-100 flex justify-center items-center flex-col 
-     overflow-hidden mx-auto sm:px-10 px-5"
+     mx-auto sm:px-10 px-5 overflow-clip"
     >
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
@@ -21,6 +23,8 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
