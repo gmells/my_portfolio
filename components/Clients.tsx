e9326a92f25bbae7844536a13ceac0516c3826ac
gmells <victormells30@gmail.com>
2024-11-04 02:59:27 +0000

    edit few words in client file

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,14 +6,16 @@ const Clients = () => {
   return (
     <div className="py-20" id="testimonials">
       <h1 className="heading">
-        Kind word from <span className="text-purple"> satisfied clients</span>
+        Kind words from <span className="text-purple"> satisfied clients</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10">
-        <InfiniteMovingCards
-          items={testimonials}
-          direction="right"
-          speed="slow"
-        />
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
